641b567bf2f525b679a6ce779e5c32b7f031f671
jesperbay <jesper.bay@live.dk>
2016-11-30 12:11:53 +0100

    NoteMVVM

---
+++ NoteMVVM1/NoteModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.MediaProperties;
+
+namespace NoteMVVM1
+{
+    class NoteModel
+    {
+        public int Nr { get; set; }
+        public string Tekst { get; set; }
+        public string Emne { get; set; }
+        public DateTime Dato { get; set; }
+
+        public static int count { get; set; }
+        public string Dag { get; set; }
+
+        public NoteModel(string tekst, string emne)
+        {
+            Nr = ++count;
+            Tekst = tekst;
+            Emne = emne;
+            Dato = DateTime.Now;
+        }
+       
+
+        public override string ToString()
+        {
+            if (Nr == 1)
+            {
+                Dag = "Mandag";
+            }
+            if (Nr == 2)
+            {
+                Dag = "Tirsdag";
+            }
+            if (Nr == 3)
+            {
+                Dag = "Onsdag";
+            }
+            if (Nr == 4)
+            {
+                Dag = "Torsdag";
+            }
+            return $"{nameof(Dag)}: {Dag}, {nameof(Tekst)}: {Tekst}, {nameof(Emne)}: {Emne}, {nameof(Dato)}: {Dato}";
+        }
+    }
+}
---
+++ NoteMVVM1/NoteViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using NoteMVVM1.Annotations;
+
+namespace NoteMVVM1
+{
+    class NoteViewModel    : INotifyPropertyChanged
+    {
+
+        public string Tekst { get; set; }
+        public string Emne { get; set; }
+        public NoteModel SelectedItem { get; set; }
+
+        public ObservableCollection<NoteModel> notes { get; set; }
+        public ObservableCollection<NoteModel> noteDone { get; set; }
+
+        public NoteViewModel()
+        {
+            notes = new ObservableCollection<NoteModel>();
+
+            notes.Add(new NoteModel("Hej", "Maybelines hofteholder"));
+            notes.Add(new NoteModel("Kan du lide det?", "hendes hofteholder"));
+            notes.Add(new NoteModel("køb det", "kage"));
+
+            noteDone = new ObservableCollection<NoteModel>();
+
+            noteDone.Add(new NoteModel( "kage", "aftensmad"));
+        }
+
+        public void Add()
+        {
+            notes.Add(new NoteModel(Tekst, Emne)); OnPropertyChanged();
+            
+        }
+
+        public void Remove()
+        {
+            notes.Remove(SelectedItem);
+            OnPropertyChanged();
+        }
+
+
+        public void Done()
+        {
+            if (SelectedItem != null)
+            {
+                noteDone.Add(SelectedItem);
+                notes.Remove(SelectedItem);
+            }
+        }
+
+        #region PropertyChangeSupport
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this , new PropertyChangedEventArgs(propertyName));
+        } 
+        #endregion
+    }
+}

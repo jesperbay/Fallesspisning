22543ca818cd5705e00e8184539a36abd1e333ca
fred3578 <fred3578@edu.easj.dk>
2016-12-08 12:21:19 +0100

    frokost

--- madapp/Footap/Maaltid.cs
+++ madapp/Footap/Maaltid.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.ApplicationModel.Appointments.AppointmentsProvider;
 
 namespace Footap
 {
@@ -16,16 +12,16 @@ namespace Footap
         public string Ret { get; set; }
         public double MadUdgift { get; set; }
 
-        public Maaltid(DateTime dategregorian, string ret, double madudgift)
+        public Maaltid (DateTime dategregorian , string ret , double madudgift)
         {
             DateGregorian = dategregorian;
             Ret = ret;
             MadUdgift = madudgift;
         }
 
-        public override string ToString()
+        public override string ToString ()
         {
             return $"{nameof(DateGregorian)}: {DateGregorian}, {nameof(Ret)}: {Ret}, {nameof(MadUdgift)}: {MadUdgift}";
         }
-    }
+}
 }
--- madapp/Footap/MaaltidViewModel.cs
+++ madapp/Footap/MaaltidViewModel.cs
@@ -9,34 +9,35 @@ using System.Threading.Tasks;
 
 namespace Footap
 {
-    class MaaltidViewModel        : INotifyPropertyChanged
-    {
-        public string Ret { get; set; }
-        public double MadUdgift { get; set; }
-        public Maaltid SelectedItem { get; set; }
 
-        public ObservableCollection<Maaltid> maaltider { get; set; }
-        
-        public MaaltidViewModel(string Ret, double MadUdgift)
+        class MaaltidViewModel : INotifyPropertyChanged
         {
-            maaltider = new ObservableCollection<Maaltid>();
-            maaltider.Add(new Maaltid(new DateTime(2016, 12, 8), "Kylling med Korhansovs", 30.5));
+            public string Ret { get; set; }
+            public double MadUdgift { get; set; }
+            public Maaltid SelectedItem { get; set; }
+
+            public ObservableCollection<Maaltid> maaltider { get; set; }
+
+            public MaaltidViewModel (string Ret , double MadUdgift)
+            {
+                maaltider = new ObservableCollection<Maaltid>();
+                maaltider.Add(new Maaltid(new DateTime(2016 , 12 , 8) , "Kylling med Korhansovs" , 30.5));
+            }
+
+            public void Add ()
+            {
+                maaltider.Add(new Maaltid(new DateTime() , Ret , MadUdgift));
+                OnPropertyChanged();
+            }
+
+
+            #region PropertyChangeSupport
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this , new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
         }
-
-        public void Add()
-        {
-            maaltider.Add(new Maaltid(new DateTime() ,Ret, MadUdgift));
-            OnPropertyChanged();
-        }
-            
-
-        #region PropertyChangeSupport
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged ([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this , new PropertyChangedEventArgs(propertyName));
-        } 
-        #endregion
     }
-}

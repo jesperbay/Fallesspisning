2a824f3b9f3659068b961e2d28ee20b4cf5ade2d
Bastian <Bastian@LAPTOP-LOOVE20V>
2016-12-13 14:06:49 +0100

    9.0

--- madapp/Footap/BeborereVm.cs
+++ madapp/Footap/BeborereVm.cs
@@ -12,30 +12,70 @@ namespace Footap
         public ObservableCollection<Beborere> Beboreres { get; set; }
         public string Name { get; set; }
         public int Alder { get; set; }
-        public int HusNr { get; set; }
+        public int HusNr01 { get; set; }
+        public ObservableCollection<Hus> Huses { get; set; }
+        public int ListHusNr { get; set; }
+
+
 
         public BeborereVm()
         {
             Beboreres = new ObservableCollection<Beborere>();
             Beboreres.Add(new Beborere("Jens", 22, 74));
+            Huses = new ObservableCollection<Hus>();
+
         }
 
+        //public int[] HusNrArray = new int[23]
+        //{
+        //     72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116
+        //};
 
-        public void Add()
+        public void mycode()
         {
-            Beboreres.Add(new Beborere(Name, Alder, HusNr));
+            List<int> HusNr02 = new List<int>();
+            HusNr02.Add(55);
         }
+       
+        
+       
+
+        //public int HusNr()
+        //{
+        //    for (int i = 0; i < HusNrArray.Length; i++)
+        //    {
+        //        return i;
+        //    }
+        ////    foreach (int Husnr in HusNrArray)
+        ////    {
+        ////        return Husnr;
+        ////    }
+        //}
+
+        //public void Add()
+        //{
+        //    Beboreres.Add(new Beborere(Name, Alder, HusNr01));
+        //}
 
         public void Remove()
         {
             foreach (Beborere beborere in Beboreres)
             {
-                if (beborere.husNr == HusNr)
+                if (beborere.husNr == HusNr01)
                 {
                     Beboreres.Remove(beborere);
                     return;
                 } 
             }
         }
+        //public override string ToString()
+        //{
+        //    for (int i = 0; i < HusNrArray.Length; i++)
+        //    {
+        //        HusNr01 = i;
+                
+        //    }
+        //    return "HusNr {0}";
+        //}
     }
 }
